41fed81e9ff68092d0ddf76f94742a74b9f55ddd
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2024-01-27 09:15:51 -0400

    feat: update models

--- models.py
+++ models.py
@@ -5,38 +5,43 @@ from sqlalchemy import (
     DateTime,
     Float,
     ForeignKey,
-    ForeignKeyConstraint,
-    Integer,
     SmallInteger,
     String,
+    Uuid,
+    text,
 )
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from datetime import date, datetime
 
 
 class Base(DeclarativeBase):
-    pass
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
 
 
 class User(Base):
     __tablename__ = "users"
 
-    uuid: Mapped[int] = mapped_column(primary_key=True)
+    uuid: Mapped[str] = mapped_column(
+        Uuid(), primary_key=True, server_default=text("uuid_generate_v4()")
+    )
     full_name: Mapped[str] = mapped_column(String(50))
     password: Mapped[str] = mapped_column(String(60))
     role: Mapped[str] = mapped_column(String(20))
     deleted: Mapped[bool] = mapped_column(Boolean)
-    created_at: Mapped[datetime] = mapped_column(DateTime)
-    updated_at: Mapped[datetime] = mapped_column(DateTime)
 
-    flight_booking: Mapped[List["FlightBooking"]] = relationship(back_populates="user")
-    room_booking: Mapped[List["RoomBooking"]] = relationship(back_populates="user")
+    flights_booking: Mapped[List["FlightBooking"]] = relationship(back_populates="user")
+    rooms_booking: Mapped[List["RoomBooking"]] = relationship(back_populates="user")
 
 
 class Flight(Base):
     __tablename__ = "flights"
 
-    uuid: Mapped[int] = mapped_column(primary_key=True)
+    uuid: Mapped[str] = mapped_column(
+        Uuid(), primary_key=True, server_default=text("uuid_generate_v4()")
+    )
     airlane_name: Mapped[str] = mapped_column(String(20))
     from_location: Mapped[str] = mapped_column(String(60))
     to_location: Mapped[str] = mapped_column(String(60))
@@ -45,33 +50,33 @@ class Flight(Base):
     total_seats: Mapped[int] = mapped_column(SmallInteger)
     price: Mapped[float] = mapped_column(Float)
     canceled: Mapped[bool] = mapped_column(Boolean)
-    created_at: Mapped[datetime] = mapped_column(DateTime)
-    updated_at: Mapped[datetime] = mapped_column(DateTime)
 
-    flight_booking: Mapped[List["FlightBooking"]] = relationship(
+    flights_booking: Mapped[List["FlightBooking"]] = relationship(
         back_populates="flight"
     )
 
 
 class FlightBooking(Base):
-    __tablename__ = "flight_booking"
+    __tablename__ = "flights_booking"
 
-    uuid: Mapped[int] = mapped_column(primary_key=True)
+    uuid: Mapped[str] = mapped_column(
+        Uuid(), primary_key=True, server_default=text("uuid_generate_v4()")
+    )
     number_seats: Mapped[int] = mapped_column(SmallInteger)
     canceled: Mapped[bool] = mapped_column(Boolean)
-    created_at: Mapped[datetime] = mapped_column(DateTime)
-    updated_at: Mapped[datetime] = mapped_column(DateTime)
-    flight_uuid: Mapped[int] = mapped_column(ForeignKey("flights.uuid"))
-    user_uuid: Mapped[int] = mapped_column(ForeignKey("users.uuid"))
+    flight_uuid: Mapped[str] = mapped_column(ForeignKey("flights.uuid"))
+    user_uuid: Mapped[str] = mapped_column(ForeignKey("users.uuid"))
 
-    flight: Mapped["Flight"] = relationship(back_populates="flight_booking")
-    user: Mapped["User"] = relationship(back_populates="flight_booking")
+    flight: Mapped["Flight"] = relationship(back_populates="flights_booking")
+    user: Mapped["User"] = relationship(back_populates="flights_booking")
 
 
 class Hotel(Base):
     __tablename__ = "hotels"
 
-    uuid: Mapped[int] = mapped_column(primary_key=True)
+    uuid: Mapped[str] = mapped_column(
+        Uuid(), primary_key=True, server_default=text("uuid_generate_v4()")
+    )
     name: Mapped[str] = mapped_column(String(50))
     country: Mapped[str] = mapped_column(String(30))
     city: Mapped[str] = mapped_column(String(40))
@@ -83,30 +88,31 @@ class Hotel(Base):
 class Room(Base):
     __tablename__ = "rooms"
 
+    uuid: Mapped[str] = mapped_column(
+        Uuid(), primary_key=True, server_default=text("uuid_generate_v4()")
+    )
     room_type: Mapped[str] = mapped_column(String(30))
     price: Mapped[float] = mapped_column(Float)
     number_guests: Mapped[int] = mapped_column(SmallInteger)
     available: Mapped[bool] = mapped_column(Boolean)
     deleted: Mapped[bool] = mapped_column(Boolean)
-    created_at: Mapped[datetime] = mapped_column(DateTime)
-    updated_at: Mapped[datetime] = mapped_column(DateTime)
-    hotel_uui: Mapped[int] = mapped_column(ForeignKey("hotels.uuid"))
+    hotel_uui: Mapped[str] = mapped_column(ForeignKey("hotels.uuid"))
 
     hotel: Mapped["Hotel"] = relationship(back_populates="rooms")
-    room_booking: Mapped[List["RoomBooking"]] = relationship(back_populates="room")
+    rooms_booking: Mapped[List["RoomBooking"]] = relationship(back_populates="room")
 
 
 class RoomBooking(Base):
-    __tablename__ = "room_booking"
+    __tablename__ = "rooms_booking"
 
-    uuid: Mapped[int] = mapped_column(primary_key=True)
+    uuid: Mapped[str] = mapped_column(
+        Uuid(), primary_key=True, server_default=text("uuid_generate_v4()")
+    )
     from_date: Mapped[date] = mapped_column(Date)
     to_date: Mapped[date] = mapped_column(Date)
     canceled: Mapped[bool] = mapped_column(Boolean)
-    created_at: Mapped[datetime] = mapped_column(DateTime)
-    updated_at: Mapped[datetime] = mapped_column(DateTime)
-    user_uuid: Mapped[int] = mapped_column(ForeignKey("users.uuid"))
-    room_uuid: Mapped[int] = mapped_column(ForeignKey("rooms.uuid"))
+    user_uuid: Mapped[str] = mapped_column(ForeignKey("users.uuid"))
+    room_uuid: Mapped[str] = mapped_column(ForeignKey("rooms.uuid"))
 
     user: Mapped["User"] = relationship(back_populates="users")
     room: Mapped["Room"] = relationship(back_populates="rooms")
